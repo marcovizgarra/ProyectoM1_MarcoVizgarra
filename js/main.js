138c6680db6f2d7bc680e16921ab6cf8e38fe86c
MarcoVizgarra_evo980 <marcovizgarra@gmail.com>
2026-08-19 17:59:00 -0300

    feat: generador de paleta de colores aleatorio incorporado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const generateBtn = document.getElementById('generate-btn');
-const paletteSizeSelect = document.getElementById('pallete-size-selector');
-const palettesContainer = document.getElementById('palettes-container');
+const paletteSizeSelect = document.getElementById('palette-size-selector');
+const palettesContainer = document.getElementById('palette-container');
 
 console.log("Generate Button:", generateBtn);
 console.log("Size Selector:", paletteSizeSelect);
 console.log("Palettes Container:", palettesContainer);
+
+const generateRandomHex = () =>{
+    const hexCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * hexCharacters.length);
+        hexColor += hexCharacters[randomIndex];
+    }
+
+    return hexColor;
+}
+
+console.log("RC1 ", generateRandomHex());
