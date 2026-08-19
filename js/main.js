250e4d9b0e20e83400a150d5659cbc5cf770ca6c
MarcoVizgarra_evo980 <marcovizgarra@gmail.com>
2026-08-19 17:47:40 -0300

    feat: vincular DOM con JS y aplicar convención kebab-case, cambio visual del selector de tamaño de paleta

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const generateBtn = document.getElementById('generate-btn');
+const paletteSizeSelect = document.getElementById('pallete-size-selector');
+const palettesContainer = document.getElementById('palettes-container');
+
+console.log("Generate Button:", generateBtn);
+console.log("Size Selector:", paletteSizeSelect);
+console.log("Palettes Container:", palettesContainer);
