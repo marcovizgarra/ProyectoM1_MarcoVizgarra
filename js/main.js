c3fd8bdfc76a8bab904030fdcefbd2be168fc444
MarcoVizgarra_evo980 <marcovizgarra@gmail.com>
2026-08-19 19:31:32 -0300

    feat: contenedor redundante eliminado en .pallete-container, generación de div con colores desde JS según cantidad elegida (no hardcodeado), función de generación de colores modificado

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ console.log("Generate Button:", generateBtn);
 console.log("Size Selector:", paletteSizeSelect);
 console.log("Palettes Container:", palettesContainer);
 
-const generateRandomHex = () =>{
+const generateRandomHexColor = () =>{
     const hexCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
     let hexColor = '#';
 
@@ -18,4 +18,47 @@ const generateRandomHex = () =>{
     return hexColor;
 }
 
-console.log("RC1 ", generateRandomHex());
+let currentPalleteSize = 6;
+
+const generatePalette = () => {
+    palettesContainer.innerHTML = '';
+    for (let i = 0; i < currentPalleteSize; i++) {
+        const randomColor = generateRandomHexColor();
+
+        const card = document.createElement('div');
+        card.classList.add('color-card');
+
+        card.style.backgroundColor = randomColor;
+        card.textContent = randomColor;
+
+        palettesContainer.appendChild(card);
+    }
+};
+
+const sizeButtons = document.querySelectorAll('#palette-size-selector button');
+
+sizeButtons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            currentPalleteSize = parseInt(event.target.textContent);
+            generatePalette();
+    });
+});
+
+generateBtn.addEventListener('click', generatePalette);
+generatePalette();
+
+
+// const generatePalette = () => {
+//     const colorCards = document.querySelectorAll('.color-card');
+
+//     colorCards.forEach(
+//         (card) => {
+//             const randomColor = generateRandomHexColor();
+//             card.style.backgroundColor = randomColor;
+//             card.textContent = randomColor;
+//         }
+//     )
+// };
+
+// generatePalette();
+// generateBtn.addEventListener('click', generatePalette);
